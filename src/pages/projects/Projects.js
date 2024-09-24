35290973cd4105bd277538c6374f49951f576793
Noureddine Ech-chouky <71594772+nour3467@users.noreply.github.com>
2024-09-24 22:06:38 +0100

    enhanced content

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -112,7 +112,7 @@ class Projects extends Component {
         <Button
           text={"More Projects"}
           className="project-button"
-          href="https://github.com/rohankokkula"
+          href="https://github.com/nour3467"
           newTab={true}
           theme={theme}
         />
--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ const seo = {
   og: {
     title: "Noureddine ECH-CHOUKY | Portfolio",
     type: "website",
-    url: "http://nour3467.com/", // Update to your actual URL.
+    url: "http://mlwithnoureddine.com/", // Update to your actual URL.
   },
 };
 
@@ -18,7 +18,7 @@ const seo = {
 
 // Home Page
 const greeting = {
-  title: "Azul!",
+  title: "Hey there!",
   sub: "Noureddine ECH.",
   logo_name: "Noureddine ECH-CHOUKY",
   resumeLink: "https://docs.google.com/document/d/1AutTKsZRZlB7HK_sJLH5diuxWablwNlh/edit?usp=sharing&ouid=113243219958794783473&rtpof=true&sd=true",
@@ -83,17 +83,18 @@ const competitiveSites = {
 const skills = {
   data: [
     {
-      title: "Data Science & AI",
+      title: "Data Science & Artificial Intelligence",
       fileName: "DataScienceImg",
       skills: [
-        "⚡ Developing scalable, production-ready models for deep learning, machine learning, and AI applications",
-        "⚡ Expertise in Computer Vision (OpenCV) and NLP, leveraging state-of-the-art models (e.g., Transformer-based)",
-        "⚡ Skilled in data cleaning, processing, and analysis using Pandas, Power BI, and advanced statistical methods",
-        "⚡ Experience in implementing AI-driven decision systems for agriculture, forecasting, and optimization",
+        "⚡ Developing, training, and deploying end-to-end machine learning models with a focus on production-ready pipelines and real-world impact",
+        "⚡ Expertise in deep learning frameworks like CNNs, RNNs, and Transformer models for advanced NLP, Computer Vision, and Reinforcement Learning applications",
+        "⚡ Strong knowledge in statistical analysis, predictive modeling, and data mining, utilizing Python, R, and scalable platforms",
+        "⚡ Proficient in data wrangling, feature engineering, and model optimization to create scalable and high-performing ML pipelines",
+        "⚡ Experience in building real-time AI-driven solutions for various industries including finance, healthcare, and e-commerce",
       ],
       softwareSkills: [
         {
-          skillName: "Tensorflow",
+          skillName: "TensorFlow",
           fontAwesomeClassname: "logos-tensorflow",
           style: {
             backgroundColor: "transparent",
@@ -107,6 +108,14 @@ const skills = {
           },
         },
         {
+          skillName: "Scikit-learn",
+          fontAwesomeClassname: "simple-icons:scikit-learn",
+          style: {
+            backgroundColor: "transparent",
+            color: "#F7931E",
+          },
+        },
+        {
           skillName: "Python",
           fontAwesomeClassname: "ion-logo-python",
           style: {
@@ -115,11 +124,19 @@ const skills = {
           },
         },
         {
+          skillName: "R",
+          fontAwesomeClassname: "simple-icons:r",
+          style: {
+            backgroundColor: "transparent",
+            color: "#276DC3",
+          },
+        },
+        {
           skillName: "Pandas",
           fontAwesomeClassname: "simple-icons:pandas",
           style: {
             backgroundColor: "transparent",
-            color: "#3776AB",
+            color: "#150458",
           },
         },
         {
@@ -130,21 +147,23 @@ const skills = {
           },
         },
         {
-          skillName: "Power BI",
-          fontAwesomeClassname: "simple-icons:powerbi",
+          skillName: "Jupyter",
+          fontAwesomeClassname: "simple-icons:jupyter",
           style: {
-            color: "#F2C811",
+            color: "#F37626",
           },
         },
       ],
     },
     {
-      title: "Cloud & DevOps",
+      title: "MLOps & Cloud Engineering",
       fileName: "CloudDevOpsImg",
       skills: [
-        "⚡ Architecting cloud solutions on AWS, specializing in cost optimization and high availability",
-        "⚡ Deploying scalable applications using Docker, Kubernetes, and CI/CD pipelines",
-        "⚡ Experience in monitoring, maintaining, and automating cloud services (AWS IAM, S3, EC2, etc.)",
+        "⚡ Designing and implementing MLOps pipelines to automate model development, version control, testing, and deployment",
+        "⚡ Automating workflows for continuous integration and continuous deployment (CI/CD), model monitoring, and scalability using Kubernetes and Docker",
+        "⚡ Experience with cloud-native AI deployments using AWS SageMaker, Google Cloud AI Platform, and Microsoft Azure Machine Learning",
+        "⚡ Expertise in integrating DevOps practices into machine learning workflows, ensuring efficient resource management and seamless scalability",
+        "⚡ Skilled in model versioning and tracking using MLOps tools like DVC, MLflow, and Git, ensuring reproducibility and efficient collaboration",
       ],
       softwareSkills: [
         {
@@ -152,6 +171,21 @@ const skills = {
           fontAwesomeClassname: "simple-icons:amazonaws",
           style: {
             color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Google Cloud",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            backgroundColor: "transparent",
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "Azure",
+          fontAwesomeClassname: "simple-icons:microsoftazure",
+          style: {
+            color: "#0089D6",
           },
         },
         {
@@ -169,10 +203,26 @@ const skills = {
           },
         },
         {
-          skillName: "CI/CD",
+          skillName: "Jenkins (CI/CD)",
           fontAwesomeClassname: "simple-icons:jenkins",
           style: {
             color: "#D24939",
+          },
+        },
+        {
+          skillName: "MLflow",
+          fontAwesomeClassname: "simple-icons:mlflow",
+          style: {
+            backgroundColor: "transparent",
+            color: "#0194E2",
+          },
+        },
+        {
+          skillName: "DVC",
+          fontAwesomeClassname: "simple-icons:dvc",
+          style: {
+            backgroundColor: "transparent",
+            color: "#945DD6",
           },
         },
       ],
@@ -181,24 +231,26 @@ const skills = {
 };
 
 
+
 /// Projects Page
 const projectsHeader = {
   title: "Projects",
   description:
-    "My projects utilize a variety of cutting-edge technologies. I specialize in Data Science, AI, and cloud-deployed solutions for scalable, impactful results.",
+    "Welcome to my project showcase, where Data Science and AI team up, and MLOps makes sure everything doesn’t fall apart. From predicting the future with algorithms (no magic, just math) to automating processes that give me more time for coffee, these projects highlight how I turn complex problems into scalable solutions. You might just find yourself wondering, 'How does that even work?'—and that’s when I know I’ve done my job right.",
 };
 
-// Project cards
+
+/// Project cards
 const projectcards = {
   list: [
     {
       title: "Bedrock Chatbot AI",
       img_path: "bedrock-chatbot.png",
       description:
-        "BedRock Chatbot AI is an advanced conversational AI application leveraging the power of Amazon Bedrock, LangChain, and Streamlit. This project demonstrates the seamless integration of state-of-the-art language models with a user-friendly interface, showcasing the potential of AI-driven conversations in various domains.",
+        "An advanced conversational AI application leveraging Amazon Bedrock, LangChain, and Streamlit. This project integrates large language models (LLMs) with a user-friendly interface, demonstrating AI-driven conversations in customer service, healthcare, and beyond. It also showcases scalable deployment with AWS Lambda and API Gateway.",
       tags: [
         {
-          lang: "python",
+          lang: "Python",
           color: "#004782",
         },
         {
@@ -210,12 +262,16 @@ const projectcards = {
           color: "#3c0095",
         },
         {
+          lang: "LangChain",
+          color: "#3498db",
+        },
+        {
           lang: "Streamlit",
           color: "#ff4b4b",
         },
         {
-          lang: "LangChain",
-          color: "#3498db",
+          lang: "API Gateway",
+          color: "#c47206",
         },
       ],
       link: "https://github.com/user-attachments/assets/31bb1f07-5d2d-4ff8-9ca5-b5916ba2ddef",
@@ -226,7 +282,7 @@ const projectcards = {
       title: "End-to-End LLM Projects (Google Gemini Series)",
       img_path: "google-gemini.jpg", // You can add a relevant image here
       description:
-        "A series of end-to-end AI projects using Google Gemini, featuring applications in document Q&A, generative AI doctors, multi-language invoice extraction, YouTube video summarization, and more. Includes shared resources for scalable and robust performance.",
+        "A series of end-to-end AI projects using Google Gemini LLMs. Includes applications such as document question-answering, AI-assisted medical diagnostics, multi-language invoice extraction, and YouTube video summarization. These projects focus on scalability, performance optimization, and integration with Google Cloud services.",
       tags: [
         {
           lang: "LLM",
@@ -240,6 +296,14 @@ const projectcards = {
           lang: "Python",
           color: "#004782",
         },
+        {
+          lang: "Google Cloud",
+          color: "#4285F4",
+        },
+        {
+          lang: "NLP",
+          color: "#00a100",
+        },
       ],
       link: "https://github.com/nour3467/End-To-End-LLM-Projects-Google-Gemini-Series",
       code: "https://github.com/nour3467/End-To-End-LLM-Projects-Google-Gemini-Series",
@@ -249,7 +313,7 @@ const projectcards = {
       title: "AWS Movie Poster Creator",
       img_path: "aws-movie-poster.png", // You can add a relevant image here
       description:
-        "AWS Movie Poster Creator leverages AWS Lambda and Bedrock's Stability Diffusion Model to generate dynamic movie posters. It stores images in S3, manages access via API Gateway, and provides secure URLs for viewing. Includes all necessary scripts and API definitions.",
+        "An automated system for creating dynamic movie posters using AWS services. This project uses AWS Lambda, Bedrock's Stability Diffusion model, and stores generated images in S3. It automates image access through API Gateway and provides secure URLs, making it a scalable solution for dynamic image generation.",
       tags: [
         {
           lang: "AWS",
@@ -263,6 +327,22 @@ const projectcards = {
           lang: "Automation",
           color: "#c47206",
         },
+        {
+          lang: "S3",
+          color: "#FF9900",
+        },
+        {
+          lang: "API Gateway",
+          color: "#c47206",
+        },
+        {
+          lang: "Lambda",
+          color: "#FF9900",
+        },
+        {
+          lang: "Stability Diffusion",
+          color: "#ff4b4b",
+        },
       ],
       link: "https://github.com/nour3467/aws-movie-poster-creator",
       code: "https://github.com/nour3467/aws-movie-poster-creator",
@@ -272,19 +352,31 @@ const projectcards = {
       title: "Fashion Recommender System",
       img_path: "fashion-recommender.png", // You can add a relevant image here
       description:
-        "A recommendation system designed to offer personalized fashion suggestions using collaborative filtering and machine learning techniques.",
+        "A machine learning-powered recommendation system that delivers personalized fashion suggestions based on user preferences. Utilizing collaborative filtering, content-based filtering, and deep learning techniques, this project showcases the power of AI in e-commerce personalization.",
       tags: [
         {
           lang: "Python",
           color: "#004782",
         },
         {
-          lang: "ML",
+          lang: "Machine Learning",
           color: "#00a100",
         },
         {
           lang: "Recommendation System",
           color: "#ff4b4b",
+        },
+        {
+          lang: "Deep Learning",
+          color: "#3498db",
+        },
+        {
+          lang: "Collaborative Filtering",
+          color: "#00a100",
+        },
+        {
+          lang: "Pandas",
+          color: "#150458",
         },
       ],
       link: "https://github.com/nour3467/Fashion-Recommender-System",
@@ -294,6 +386,7 @@ const projectcards = {
   ],
 };
 
+
 const resumeleft = {
   left: {
     title: ["Experience"],
@@ -301,28 +394,31 @@ const resumeleft = {
       {
         title: "Data Scientist",
         subtitle: "Mohammed VI Polytechnic University",
-        date: "Aug 2023 - Present",
+        date: "Aug 2023 - Present | Benguerir, MA",
         content: [
-          "Engineered AI recommendation system using StableBaseline3 and WandB, reducing resource usage by 15% and increasing profits by 10%.",
-          "Developed predictive models with Scikit-learn and XGBoost, improving yield forecast accuracy by 25%.",
-          "Led AWS migration (IAM, CloudWatch, S3, ASG, Route53), reducing system downtime by 99.9% and cutting costs by 30%.",
-          "Mentored and contributed to 'AgriAssist' AI agent using Rasa, RAG and LaMa2 LLM, increasing user engagement by 45%.",
-          "Delivered 20+ presentations using PowerBI and SHAP, resulting in 40% more project approvals.",
-          "Expanded solutions to 3 countries, securing 3 new partnerships and $2M increase in annual revenue.",
+          "Engineered an AI recommendation system using StableBaseline3 and Weights & Biases (WandB), reducing resource usage by 15% and increasing profits by 10%.",
+          "Developed predictive models with Scikit-learn and XGBoost, improving yield forecast accuracy by 25% and reducing crop losses by 20%.",
+          "Analyzed diverse data sources including weather data, soil data, stakeholder-collected information, and remote sensing imagery to enhance agricultural decision-making and improve crop management strategies.",
+          "Led AWS migration (IAM, CloudWatch, S3, ASG, Route53), reducing system downtime by 99.9%, cutting costs by 30%, and completing the project two weeks ahead of schedule.",
+          "Mentored and contributed to the 'AgriAssist' AI agent using Rasa, Retrieval-Augmented Generation (RAG), and LaMa2 LLM, increasing user engagement by 45% and reducing query resolution time by 60%.",
+          "Delivered over 20 presentations using PowerBI and SHAP, resulting in a 40% increase in project approvals and 25% higher client retention.",
+          "Expanded solutions to 3 countries, securing 3 new partnerships and driving a $2M increase in annual revenue.",
         ],
       },
       {
         title: "Data Scientist (Internship)",
         subtitle: "Mohammed VI Polytechnic University",
-        date: "June 2022 - Aug 2022",
+        date: "June 2022 - Aug 2022 | Benguerir, MA",
         content: [
-          "Developed a Wheat Yield Prediction Toolkit using deep learning with Pytorch, TensorFlow, and Keras.",
-          "Built a two-branch (LSTM-CNN) model, improving model accuracy and conducting uncertainty analysis with Bayesian methods.",
+          "Developed a Wheat Yield Prediction Toolkit using deep learning frameworks such as PyTorch, TensorFlow, and Keras, improving yield prediction accuracy by 30%.",
+          "Designed and implemented a two-branch (LSTM-CNN) model, increasing overall model performance by 25% and conducting uncertainty analysis with Bayesian methods, reducing model error margins by 20%.",
+          "Analyzed historical crop data, weather patterns, and satellite imagery to optimize agricultural forecasting, helping reduce resource misallocation by 15%.",
         ],
       },
     ],
   },
 };
+
 
 const resumeright = {
   right: [
@@ -331,9 +427,13 @@ const resumeright = {
       data: [
         {
           title: "Engineering Degree in Data Science and AI",
-          subtitle: "Mohammed V University (ENSAMR Rabat)",
+          subtitle: "Mohammed V University (ENSAM Rabat)",
           date: "Jan 2020 - Aug 2023",
-          content: ["Concentration: Data Science, AI & Big Data Analytics"],
+          content: [
+            "Specialization: Data Science, Artificial Intelligence, and Big Data Analytics",
+            "Relevant coursework: Machine Learning, Deep Learning, MLOps, Cloud Computing, and Data Engineering",
+            "Graduated with Distinction, Top 5% of class",
+          ],
         },
       ],
     },
@@ -343,9 +443,10 @@ const resumeright = {
         {
           title: "Key Achievements",
           content: [
-            "Led AI system development reducing resource usage by 15% and increasing profits by 10%",
-            "Spearheaded AWS migration, reducing system downtime by 99.9% and cutting costs by 30%",
-            "Expanded data-driven solutions to 3 countries, securing $2M increase in annual revenue",
+            "Led development of an AI recommendation system, reducing resource usage by 15% and boosting profits by 10%",
+            "Spearheaded a successful AWS migration, reducing system downtime by 99.9% and cutting costs by 30%",
+            "Expanded data-driven solutions to 3 international markets, resulting in $2M increase in annual revenue",
+            "Recipient of the 'Outstanding Innovator Award' for pioneering AI-driven solutions in agriculture",
           ],
         },
       ],
@@ -356,15 +457,18 @@ const resumeright = {
         {
           title: "AI Consultant",
           subtitle: "Various Clients",
+          date: "Jan 2021 - Present",
           content: [
-            "Provided expert advice on implementing AI solutions in agricultural technology",
-            "Developed custom machine learning models for specific agricultural use cases",
+            "Provided strategic advice on implementing AI solutions to optimize agricultural technology processes and resource management",
+            "Developed custom machine learning models tailored for agricultural use cases, improving predictive accuracy by up to 30%",
+            "Collaborated with startups to design scalable AI workflows, reducing time-to-market by 20%",
           ],
         },
       ],
     },
   ],
 };
+
 
 
 //certificate cards
@@ -389,7 +493,7 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "contact.jpg",
     description:
-      "I'm always open to discussing data science projects, AI innovations, or potential collaborations. Feel free to reach out to me through any of the channels below. I specialize in machine learning, deep learning, and cloud-based AI solutions.",
+      "Got a data science project, a wild AI idea, or just want to talk about how machines are slowly taking over the world (in a good way)? I’m all ears! Whether it’s machine learning, deep learning, or cloud-based AI wizardry, feel free to reach out through any of the channels below. Let’s make something awesome together!",
   },
   addressSection: {
     title: "Location",
@@ -424,16 +528,7 @@ const socialMediaLinks = [
   },
 ];
 export {
-  settings,
-  seo,
-  greeting,
-  socialMediaLinks,
-  skills,
-  competitiveSites,
-  projectsHeader,
-  contactPageData,
-  projectcards,
-  certifications,
-  resumeleft,
-  resumeright,
+  certifications, competitiveSites, contactPageData, greeting, projectcards, projectsHeader, resumeleft,
+  resumeright, seo, settings, skills, socialMediaLinks
 };
+
